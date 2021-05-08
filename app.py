9b9c6834a492b49a13a7f8d2e9439875dc58be9c
Ricardo Uribe <ricardoauribe@hotmail.com>
2021-05-08 12:10:49 -0500

    Adding initial options and arguments

--- app.py
+++ app.py
@@ -1,2 +1,23 @@
-def cli():
-    print('Hey')
+import click
+
+@click.command()
+@click.option('--interface', default='api', 
+    help='Input to indicate interaction method (api, event, ui) ')
+@click.option('--language', default='javascript', 
+    help='Input desired target language (java, python, go, javascript) ')
+@click.option('--storage', default='none', 
+    help='Input desired storage layer (s3, dynamodb, postgres, redis) ')
+@click.argument('name')
+def cli(name, interface, language, storage):
+    """ Galvanize CLI to create base microservices """
+    click.echo('Creating ' + name + ' microservice with options:')
+    click.echo('--interface ' + interface)
+    click.echo('--language ' + interface)
+    click.echo('--storage ' + storage)
+
+    # 1. Validate options passed by the user are valid
+    # 2. Based on the language and core configuration options go to github and 
+    #    retrieve the proper project template 
+    # 3. Based on selected add on modify the service to add queue or storage 
+    #    scaffold connections
+
